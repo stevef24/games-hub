1f2e9eba0df4e13aaf01242aac2d39dca3477a48
Stav Fernandes <stevefernandes24@hotmail.co.uk>
2023-04-21 12:34:06 +0100

    Built responsive layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,25 @@
-import { Button } from "@chakra-ui/react";
+import { Grid, GridItem, Show } from "@chakra-ui/react";
 
 function App() {
 	return (
-		<div className="App">
-			<Button>hello world</Button>
-		</div>
+		<Grid
+			templateAreas={{
+				base: `"nav" "main"`,
+				lg: `"nav nav" "aside main"`,
+			}}
+		>
+			<GridItem area="nav" bg="gray.100">
+				nav
+			</GridItem>
+			<Show above="lg">
+				<GridItem area="aside" bg="gray.200">
+					aside
+				</GridItem>
+			</Show>
+			<GridItem area="main" bg="gray.300">
+				main
+			</GridItem>
+		</Grid>
 	);
 }
 
