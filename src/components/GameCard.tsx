e42d4eec14bf26d4802a7c061e8c4c5aa4432f27
Stav Fernandes <stevefernandes24@hotmail.co.uk>
2023-04-21 15:46:18 +0100

    git optimised  image

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,6 +1,9 @@
 import React from "react";
 import { Game } from "./hooks/useGame";
-import { Card, CardBody, Heading, Image } from "@chakra-ui/react";
+import { Card, CardBody, Heading, Image, HStack } from "@chakra-ui/react";
+import PlatformIconList from "./PlatformIconList";
+import CriticScore from "./CriticScore";
+import getCroppedImageUrl from "../services/image-url";
 
 type Props = {
 	game: Game;
@@ -9,9 +12,15 @@ type Props = {
 const GameCard = ({ game }: Props) => {
 	return (
 		<Card borderRadius={8} overflow={"hidden"}>
-			<Image src={game.background_image} />
+			<Image src={getCroppedImageUrl(game.background_image)} />
 			<CardBody>
 				<Heading fontSize={"2xl"}>{game.name}</Heading>
+				<HStack justifyContent={"space-between"}>
+					<PlatformIconList
+						platforms={game.parent_platforms.map((p) => p.platform)}
+					/>
+					<CriticScore score={game.metacritic} />
+				</HStack>
 			</CardBody>
 		</Card>
 	);
--- src/components/hooks/useGame.ts
+++ src/components/hooks/useGame.ts
@@ -2,10 +2,18 @@ import { useEffect, useState } from "react";
 import apiClient from "../../services/api-client";
 import { CanceledError } from "axios";
 
+export type Platform = {
+	id: number;
+	name: string;
+	slug: string;
+};
+
 export type Game = {
 	id: number;
 	name: string;
 	background_image: string;
+	parent_platforms: { platform: Platform }[];
+	metacritic: number;
 };
 
 type FetchGamesResponse = {
