5d08d12ea0c91423b128b72abb677de43feecfc8
Stav Fernandes <stevefernandes24@hotmail.co.uk>
2023-04-24 12:15:40 +0100

    created genre hook

---
+++ src/components/GenreList.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import useGenres from "./hooks/useGenres";
+
+const GenreList = () => {
+	const { genreList } = useGenres();
+
+	return (
+		<ul>
+			{genreList.map((genre) => (
+				<li key={genre.id}>{genre.name}</li>
+			))}
+		</ul>
+	);
+};
+
+export default GenreList;
