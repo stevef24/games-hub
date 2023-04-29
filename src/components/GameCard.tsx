10895ed08f0d4a786711a1d8b24a58d792362383
Stav Fernandes <stevefernandes24@hotmail.co.uk>
2023-04-29 12:12:27 +0100

    added emojies

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -4,6 +4,7 @@ import { Card, CardBody, Heading, Image, HStack } from "@chakra-ui/react";
 import PlatformIconList from "./PlatformIconList";
 import CriticScore from "./CriticScore";
 import getCroppedImageUrl from "../services/image-url";
+import Emoji from "./Emoji";
 
 type Props = {
 	game: Game;
@@ -14,13 +15,15 @@ const GameCard = ({ game }: Props) => {
 		<Card>
 			<Image src={getCroppedImageUrl(game.background_image)} />
 			<CardBody>
-				<Heading fontSize={"2xl"}>{game.name}</Heading>
-				<HStack justifyContent={"space-between"}>
+				<HStack justifyContent={"space-between"} marginBottom={3}>
 					<PlatformIconList
 						platforms={game.parent_platforms.map((p) => p.platform)}
 					/>
 					<CriticScore score={game.metacritic} />
 				</HStack>
+				<Heading fontSize={"2xl"}>
+					{game.name} <Emoji rating={game.rating_top} />
+				</Heading>
 			</CardBody>
 		</Card>
 	);
